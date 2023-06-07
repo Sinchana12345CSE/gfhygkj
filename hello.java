2377eacaaac25acbf9fb59a59a4ec7a5559d8232
Sinchana12345CSE <129932812+Sinchana12345CSE@users.noreply.github.com>
2023-06-07 12:52:45 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class helloworld{
 	public static void main(String []args)
 	{
 		System.out.println("hello world")
-			System.out.println("welcome to spt")
+			System.out.println("welcome to spt fro tumkuru")
 
 	}
 }
